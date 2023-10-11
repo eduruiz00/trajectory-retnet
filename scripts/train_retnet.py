709612e9179b0c6d050143363c07c59aace97310
Mireia Planas Lisbona <mplalis99@gmail.com>
2023-10-11 12:28:14 +0200

    first attempt in combining papers

---
+++ scripts/train_retnet.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+import torch
+import pdb
+
+import trajectory.utils as utils
+import trajectory.datasets as datasets
+from trajectory.models.transformers import GPT
+from torchscale.architecture.config import RetNetConfig
+from torchscale.architecture.retnet import RetNetDecoder
+
+class Parser(utils.Parser):
+    dataset: str = 'halfcheetah-medium-expert-v2'
+    config: str = 'config.offline'
+
+#######################
+######## setup ########
+#######################
+
+args = Parser().parse_args('train')
+
+#######################
+####### dataset #######
+#######################
+
+env = datasets.load_environment(args.dataset)
+
+sequence_length = args.subsampled_sequence_length * args.step
+
+dataset_config = utils.Config(
+    datasets.DiscretizedDataset,
+    savepath=(args.savepath, 'data_config.pkl'),
+    env=args.dataset,
+    N=args.N,
+    penalty=args.termination_penalty,
+    sequence_length=sequence_length,
+    step=args.step,
+    discount=args.discount,
+    discretizer=args.discretizer,
+)
+
+dataset = dataset_config()
+obs_dim = dataset.observation_dim
+act_dim = dataset.action_dim
+transition_dim = dataset.joined_dim
+
+#######################
+######## model ########
+#######################
+
+block_size = args.subsampled_sequence_length * transition_dim - 1
+print(
+    f'Dataset size: {len(dataset)} | '
+    f'Joined dim: {transition_dim} '
+    f'(observation: {obs_dim}, action: {act_dim}) | Block size: {block_size}'
+)
+
+model_config = RetNetConfig(vocab_size=64000)
+
+model = RetNetDecoder(model_config)
+model.to(args.device)
+
+#######################
+####### trainer #######
+#######################
+
+warmup_tokens = len(dataset) * block_size ## number of tokens seen per epoch
+final_tokens = 20 * warmup_tokens
+
+trainer_config = utils.Config(
+    utils.Trainer,
+    savepath=(args.savepath, 'trainer_config.pkl'),
+    # optimization parameters
+    batch_size=args.batch_size,
+    learning_rate=args.learning_rate,
+    betas=(0.9, 0.95),
+    grad_norm_clip=1.0,
+    weight_decay=0.1, # only applied on matmul weights
+    # learning rate decay: linear warmup followed by cosine decay to 10% of original
+    lr_decay=args.lr_decay,
+    warmup_tokens=warmup_tokens,
+    final_tokens=final_tokens,
+    ## dataloader
+    num_workers=0,
+    device=args.device,
+)
+trainer = trainer_config()
+
+#######################
+###### main loop ######
+#######################
+
+## scale number of epochs to keep number of updates constant
+n_epochs = int(1e6 / len(dataset) * args.n_epochs_ref)
+save_freq = int(n_epochs // args.n_saves)
+
+for epoch in range(n_epochs):
+    print(f'\nEpoch: {epoch} / {n_epochs} | {args.dataset} | {args.exp_name}')
+
+    trainer.train(model, dataset)
+
+    ## get greatest multiple of `save_freq` less than or equal to `save_epoch`
+    save_epoch = (epoch + 1) // save_freq * save_freq
+    statepath = os.path.join(args.savepath, f'state_{save_epoch}.pt')
+    print(f'Saving model to {statepath}')
+
+    ## save state to disk
+    state = model.state_dict()
+    torch.save(state, statepath)
